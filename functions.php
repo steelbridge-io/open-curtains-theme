9f0c7003b1e43695dc5b23cfb81b2927cc48fff8
Chris Parsons <chris@parsonshosting.com>
2022-09-04 23:00:37 -0700

    CSS updates

--- functions.php
+++ functions.php
@@ -90,3 +90,13 @@ function wppbc_current_user_link( $atts, $content ) {
 
 	return ;
 }
+
+/**
+ * Logout redirect
+ */
+add_action('wp_logout','auto_redirect_after_logout');
+
+function auto_redirect_after_logout(){
+ wp_safe_redirect( home_url() );
+ exit;
+}
