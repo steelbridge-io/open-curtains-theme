ef7eb99d9ff827ef95816830f63ddda9628e356d
Chris Parsons <chris@parsonshosting.com>
2022-09-27 13:19:02 -0700

    Added custom js for updates to .text

---
+++ assets/js/custom.js
@@ -0,0 +1,8 @@
+/** Custom JS */
+
+var $ = jQuery.noConflict();
+
+$(document).ready(function() {
+    /* Update text associated with Post button in the WP Front End plugin */
+    $('.wpuf-menu-item.post a').text('Casting Calls');
+});
